39301d8d1651386216b9bb1c41ef79caa5b757f6
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-04-30 16:01:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -59,15 +59,6 @@
         </div>
 
         <!-- Script de verificacion de formulario -->
-
-        <?php
-            $servidor = "localhost";
-            $usuarioBD = "root";
-            $contraseña = "";
-            $baseDeDatos = "bodega";
-            $conexion = mysqli_connect($servidor, $usuarioBD, $contraseña, $baseDeDatos) or die ("La conexion al servidor no fue establecida");
-            $db = mysqli_select_db($conexion,$baseDeDatos) or die ("No se logro conectar a la base de datos");
-        ?>
         <script>
             const app = new Vue({
                 el: '#formulario',
