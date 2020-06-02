cd79e2a0816897c4d917217a36488122524b2d1e
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-06-02 12:19:29 -0500

    Nueva estructura mediante NPM

--- conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servidor = "localhost";
-$usuarioBD = "root";
-$contraseña = "";
-$baseDeDatos = "recetas";
-$conexion = mysqli_connect($servidor, $usuarioBD, $contraseña, $baseDeDatos) or die ("La conexion al servidor no fue establecida");
-$db = mysqli_select_db($conexion,$baseDeDatos) or die ("No se logro conectar a la base de datos");
-?>
